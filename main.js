46b164b963259a0e1df5009bdcadacb00b83d0b9
annavelk <90451059+annavelk@users.noreply.github.com>
2021-09-14 01:11:48 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function getRandomIntInclusive() {
+function RandomInt() {
   min = Math.ceil(1);
   max = Math.floor(10);
   a = Math.floor(Math.random() * (max - min + 1)) + min;
@@ -8,15 +8,16 @@ function getRandomIntInclusive() {
   element.value = a;
    return a;
 }
-console.log(getRandomIntInclusive());
 
 
-function nextnum() {
-let input = document.getElementById('input_id');
-  let b = input++;
+
+function nextnum(f) {
+  input = f.input_id.value;
+ b = input + 1;
 console.log(b);
  element2 = document.getElementById('result2');
  console.log(element2);
   element2.value = b;
   return b;
 }
+ 
